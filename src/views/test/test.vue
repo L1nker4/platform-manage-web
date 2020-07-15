2548fed8c4c8cc810c855329ef88b12ca22860ed
l1nker4 <l1nker4@outlook.com>
2020-07-15 16:17:42 +0800

    Initial commit

---
+++ src/views/test/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">{{hello}}</div>
+</template>
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      hello: 'hello world'
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+  .test {
+    color: #42b984;
+    font-size: 1.1rem;
+    font-weight: 600;
+  }
+</style>
